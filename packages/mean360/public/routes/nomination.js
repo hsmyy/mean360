0bff0e33446a54e1004de87b5d38036ca20a8ac2
Wang Jingfei <wjf.wap@gmail.com>
2014-09-04 01:42:35 +0800

    Add controller for evaluation.

--- packages/mean360/public/routes/nomination.js
+++ packages/mean360/public/routes/nomination.js
@@ -1,18 +1,18 @@
 'use strict';
 
-angular.module('mean.mean360').config(
-		[ '$stateProvider', function($stateProvider) {
-			$stateProvider.state('选择评价者', {
-				url : '/nomination/',
-				templateUrl : 'mean360/views/nomi.html'
-			}).state('评估', {
-				url : '/evaluation/',
-				templateUrl : 'mean360/views/eval.html'
-			}).state('反馈', {
-				url : '/feedback/',
-				templateUrl : 'mean360/views/feedback.html'
-			}).state('管理',{
-				url : '/manage/',
-				templateUrl : 'mean360/views/manage.html'
-			});
-		} ]);
+angular.module('mean.mean360').config([ '$stateProvider', function ($stateProvider) {
+    $stateProvider.state('选择评价者', {
+        url: '/nomination/',
+        templateUrl: 'mean360/views/nomi.html'
+    }).state('评估', {
+        url: '/evaluation/',
+        templateUrl: 'mean360/views/eval.html'
+    }).state('反馈', {
+        url: '/feedback/',
+        templateUrl: 'mean360/views/feedback.html'
+    }).state('管理', {
+        url: '/manage/',
+        templateUrl: 'mean360/views/manage.html'
+    });
+} ]);
+
