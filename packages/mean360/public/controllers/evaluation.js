a179925a31a1094148e159463d85620163283abf
Rey <chen_m@worksap.co.jp>
2014-09-04 15:05:19 +0800

    commit

--- packages/mean360/public/controllers/evaluation.js
+++ packages/mean360/public/controllers/evaluation.js
@@ -11,7 +11,26 @@ angular.module('mean.mean360').controller('EvaluationController', ['$scope', '$h
         $scope.rating = 5;
 
 
-        $http.get('questions').success(function (data) {
+
+//    $scope.questions = [                    {
+//                        'id' : '1',
+//                        'category' : '洞察力',
+//                        'question' : '可以提出有力的問題，使複雜問題清晰化'
+//                        },{
+//                        	'id' : '2',
+//                        	'category' :'洞察力',
+//                        	'question' : '指出討論中的漏洞或錯誤'
+//                        },{
+//                        	'id' : '3',
+//                        	'category' : '洞察力',
+//                        	'question' : '解決問題時，快速發現問題的關鍵原因'
+//                        },{
+//                        	'id' : '4',
+//                        	'category' : '前瞻性',
+//                        	'question' : '分享行業前沿信息'
+//                        }];
+
+        $http.get('query/question/all').success(function (data) {
             $scope.questions = data;
         }).error(function (data, status) {
             $scope.questions = [
@@ -39,7 +58,20 @@ angular.module('mean.mean360').controller('EvaluationController', ['$scope', '$h
         });
 
 
-        $http.get('candidates').success(function (data, status) {
+        // TEST ONLY. Replace this with ajax request.
+//        $scope.candidates = [
+//            {
+//                img: 'mean360/assets/img/account.jpg',
+//                name: '自己'
+//            },
+//            {
+//                img: 'mean360/assets/img/account.jpg',
+//                name: '关羽',
+//                position: '大将军',
+//                department: '荆州'
+//            }
+//        ];
+        $http.get('get_evaluatees').success(function (data, status) {
             $scope.candidates = data;
         }).error(function (data, status) {
             // TODO Handler errors here.
