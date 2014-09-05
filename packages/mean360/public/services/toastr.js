1cf13722b81423b9b2b41775368e700d12585679
Fify <wang_j@worksap.co.jp>
2014-09-05 11:51:11 +0800

    Add missing file.

---
+++ packages/mean360/public/services/toastr.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('mean.mean360').factory('toastr', [
+    function () {
+        function show(type, position, message) {
+            toastr.options.positionClass = position;
+            toastr[type][message];
+        }
+
+        function info(position, message) {
+            show('info', position, message);
+        }
+
+        function error(position, message) {
+            show('error', position, message);
+        }
+
+        function warning(position, message) {
+            show('warning', position, message);
+        }
+
+        function success(position, message) {
+            show('success', position, message);
+        }
+
+        return {
+            toaster: show,
+            info: info,
+            success: success,
+            warning: warning,
+            error: error
+        };
+    }
+]);
