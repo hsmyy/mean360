628e30e0efc411d07a1d89989e672a386689dd76
Wang Jingfei <wjf.wap@gmail.com>
2014-09-21 01:06:39 +0800

    Move the evaluation model setting to the setting page.

--- packages/mean360/public/controllers/manager.js
+++ packages/mean360/public/controllers/manager.js
@@ -30,84 +30,6 @@ angular.module('mean.mean360').controller(
                 }
             ];
 
-            $scope.category = ['戰略執行', '前瞻預測', '目標意識'];
-            $scope.newCategory = '愿景规划';
-            $scope.choosedCategory = 0;
-            $scope.newQuestion = '能够很好地预测行业未来发展方向';
-
-            var data = [
-                {
-                    category: '戰略執行',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '戰略執行',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '戰略執行',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '戰略執行',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '前瞻預測',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '前瞻預測',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '前瞻預測',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '目標意識',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '目標意識',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作',
-                    content: '123'
-                },
-                {
-                    category: '目標意識',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '團隊合作',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '團隊合作',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                },
-                {
-                    category: '團隊合作',
-                    question: '善于建立和维持广泛、友好的人际关系来帮助开展工作'
-                }
-            ];
-
-            $scope.tableParams = new NgTableParams(
-                {
-                    page: 1, // show first page
-                    count: 10
-                    // count per page
-                },
-                {
-                    groupBy: 'category',
-                    total: data.length,
-                    getData: function ($defer, params) {
-                        var orderedData = params.sorting() ? $filter('orderBy')(data, $scope.tableParams.orderBy()) : data;
-
-                        $defer.resolve(orderedData.slice(
-                                (params.page() - 1) * params.count(),
-                                params.page() * params.count()));
-                    }
-                });
             $scope.startPeriod = function (index) {
                 toastr.success('toast-top-right', $scope.timeline[index].name + '开始');
             };
@@ -122,25 +44,6 @@ angular.module('mean.mean360').controller(
 
             $scope.save = function () {
                 toastr.success('toast-top-right', '保存成功！');
-            };
-
-            $scope.createCategory = function () {
-                var newCate = $scope.newCategory.trim();
-                if (newCate.length > 0) {
-                    $scope.category.push(newCate);
-                }
-            };
-
-            $scope.createQuestion = function () {
-                var cate = $scope.category[$scope.choosedCategory];
-                var newQues = $scope.newQuestion.trim();
-                if (newQues.length > 0) {
-                    data.push({
-                        category: cate,
-                        question: newQues
-                    });
-                    $scope.tableParams.reload();
-                }
             };
 
             $scope.openCalendar = function($event, input) {
